e61e0eaa0a2759fa0dbd7bc49ab5e616442590f9
TaeHyung KIM <th1122kim@gmail.com>
2022-08-22 15:31:51 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  > button {
+    width: 50px;
+  }
+`;
 
 function App() {
   return (
@@ -7,10 +15,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Test</p>
-        <div>
+        <Container className="button_container">
           <button>+</button>
           <button>-</button>
-        </div>
+        </Container>
       </header>
     </div>
   );
